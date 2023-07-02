d152c3fa780c28150f79fa81a8b49d6b171d2d0e
Carsten Allefeld <carsten.allefeld@city.ac.uk>
2023-07-02 20:29:18 +0100

    updated version and copyright

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # MVPA by Cross-Validated (Cross-) MANOVA
 
 This project is under development and not yet usable.
+
+
+***
+
+This software is copyrighted © 2023 by Carsten Allefeld and released under the terms of the GNU General Public License, version 3 or later, with contributions by Remi Gau, Polina Iamshchinina, and Thomas Christophel.
+
+It has been developed with Matlab R2022b (v9.13) and SPM12 r7771, but should work with later and possibly slightly earlier versions.
--- toolbox/Contents.m
+++ toolbox/Contents.m
@@ -1,7 +1,13 @@
-% cvmanova
-% Version 4.0.0-alpha 2023-06-29
+% cvcrossmanova
+% Version 0.0.0 2023-06-29
 %
 % MVPA by Cross-Validated (Cross-) MANOVA
 %
-% For more information, please see the included file 'README.md'
-% or visit <https://github.com/allefeld/cvmanova/>.
+% For more information, please <https://github.com/allefeld/cvcrossmanova/>.
+%
+% This software is copyrighted © 2023 by Carsten Allefeld and released under
+% the terms of the GNU General Public License, version 3 or later, with
+% contributions by Remi Gau, Polina Iamshchinina, and Thomas Christophel.
+%
+% It has been developed with Matlab R2022b (v9.13) and SPM12 r7771, but should
+% work with later and possibly slightly earlier versions.
